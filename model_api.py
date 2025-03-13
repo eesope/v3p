6ad7e12d0a371ba9f5a4d58ea5d2ec1510208aa6
Saeyoung Park <spark363@my.bcit.ca>
2025-03-13 00:13:31 -0700

    Update to lazy load

--- model_api.py
+++ model_api.py
@@ -5,16 +5,26 @@ import os
 app = FastAPI()
 model_path = os.path.abspath("./mpt-7b-storywriter")
 os.environ["HUGGINGFACE_HUB_CACHE"] = model_path
-
 model_name = "mosaicml/mpt-7b-storywriter"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# to make it lazily load
+tokenizer = None
+model = None
+
+def load_model():
+    global tokenizer, model
+    if tokenizer is None or model is None:
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name)
+    return tokenizer, model
 
 @app.get("/generate_story")
 async def generate_story(prompt: str = "Three cats are"):
+    # 첫 요청 시 모델과 토크나이저를 로드합니다.
+    tokenizer, model = load_model()
+    
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids
     output = model.generate(input_ids, max_length=5, num_return_sequences=1)
-
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     
     return {
@@ -22,11 +32,8 @@ async def generate_story(prompt: str = "Three cats are"):
         "generated_text": generated_text
     }
 
-# you may need to type command if requirements.txt not work fully
-# pip install uvicorn
-
-# start server:
+# manual start server:
 # uvicorn model_api:app --reload --log-level debug
 
 # query via url
-# http://url:8000/generate_story?prompt=Three%20cats%20are
+# http://url/generate_story?prompt=Three%20cats%20are
