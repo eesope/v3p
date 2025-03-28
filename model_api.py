5a4516cc22c025a2be56f38dec281fe181efe234
Saeyoung Park <spark363@my.bcit.ca>
2025-03-28 11:20:06 -0700

    Change LLM model

--- model_api.py
+++ model_api.py
@@ -1,12 +1,17 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+# to run on cpu
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
 import os
 import time
 
-model_path = os.path.abspath("./mpt-7b-storywriter")
+# https://huggingface.co/google/flan-t5-base
+model_path = os.path.abspath("./")
 os.environ["HUGGINGFACE_HUB_CACHE"] = model_path
-model_name = "mosaicml/mpt-7b-storywriter"
+model_name = "google/lan-t5-base"
 
 # to make it lazily load
 tokenizer = None
@@ -17,8 +22,8 @@ async def lifespan(app: FastAPI):
     global tokenizer, model
     print("Loading model and tokenizer...")
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
+    model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
     print("Model and tokenizer loaded.")
     yield # server executing... 
 
@@ -29,22 +34,27 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.get("/generate_txt")
+@app.get("/t2t")
 async def generate_txt(prompt: str):   
+
+    input_text = "translate English to French: What is your name?"
     # prompt = data.get("prompt", "Three cats are") # if @app.post
     start_inference = time.perf_counter()
 
-    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-    output = model.generate(input_ids, max_length=5, num_return_sequences=1)
+    input_ids = tokenizer(input_text, return_tensors="pt").input_ids
 
-    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    outputs = model.generate(input_ids)
+    result = tokenizer.decode(outputs[0])
+
+    # output = model.generate(prompt, max_length=5, num_return_sequences=1)
+    # generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     
     inference_time = time.perf_counter() - start_inference
     print(f"Inference for prompt '{prompt}' took {inference_time:.2f} seconds.")
     
     return {
-        "prompt": prompt,
-        "generated_text": generated_text,
+        "prompt": input_text,
+        "generated_text": result,
         "inference_time": inference_time
     }
 
